38ede48b1b404cee7674919ba1582a229423f420
Margaretha05 <margaretharuth5@gmail.com>
2025-06-18 23:31:42 +0700

    update list TS UI Modal

--- Test Suites/TS_UI_Modal.ts
+++ Test Suites/TS_UI_Modal.ts
@@ -12,31 +12,31 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>74028af6-91a2-44e4-8226-00de5b84a566</testSuiteGuid>
    <testCaseLink>
-      <guid>41476ada-b9de-4e99-bd76-508441dea506</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Alerts, Frame and Windows/Modal/test_close_large_modal_success</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>67168289-a238-4618-83d9-47005b6b0ad2</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Alerts, Frame and Windows/Modal/test_close_small_modal_success</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>928d2e36-14ef-4e01-9baf-ae71e071271b</guid>
+      <guid>c089e063-6819-4b83-9858-cb4df36d2117</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Alerts, Frame and Windows/Modal/test_open_large_modal_displayed</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>7f012aea-a802-4469-9be7-9d6cb6186ad9</guid>
+      <guid>e4340f32-bcdf-4674-b77a-60a31c6632fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Alerts, Frame and Windows/Modal/test_close_large_modal_success</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>431cba07-2953-430e-8b65-6ac8be7057a4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Alerts, Frame and Windows/Modal/test_open_small_modal_displayed</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>348e9d08-1189-420a-bb6a-71f638a8c409</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Alerts, Frame and Windows/Modal/test_close_small_modal_success</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
